b5217dc3d77d766a57e9a8ea6567d11eea66cc6e
George Rozitis <namacker.fire@gmail.com>
2021-11-08 17:15:21 -0800

    Created game map and can move player

--- game.py
+++ game.py
@@ -13,49 +13,6 @@ Questions since Chris took off our "training wheels":
 - Can we use classes?
 """
 
-"""
-#=========================#
-|F44O444444444444444444444|
-|========================.|
-|.........................|
-|...........#============T|
-|...........|3333333333333|
-|...........|3333333333333|
-|...........|3333333333333|
-|...........|OOOOO33333333|
-|...........|3333O33333333|
-|...........|B333O33333333|
-|T======#...#=============|
-|2222222|.................|
-|2222222|.................|
-|2222222|.................|
-|2222222|.................|
-|2222222|.................|
-|OOOOOOO|.................|
-|2222222|.................|
-|2222222|.................|
-|222222B|.................|
-|========================B|
-|00001111111111O1111111111|
-|00T01111111111O1111111111|
-|00001111111111O1111111111|
-|00001111111111O1111111111|
-#=========================#
-
----Legend---
-. - Main Area
-0 - Starting Area
-1 - Dungeon 1 (Underground Area)
-2 - Dungeon 2 (Colorless Dungeon)
-3 - Dungeon 3 (Mountain)
-4 - Dungeon 4 (The Dark World)
-
-B - Boss
-F - Final Boss
-O - Organ Man Event
-T - Town (Buy, Inn)
-"""
-
 
 # ~~~ CONSTANTS ~~~
 ENCOUNTER_RATE = 20
@@ -109,6 +66,7 @@ enemies = {
 }
 
 colors_dict = {
+    'b_black': '\033[40m',
     'c_reset': '\033[0m',
     'c_black': '\033[30m',
     'c_red': '\033[91m',
@@ -160,7 +118,7 @@ def make_character():
     """
     Return the player character.
 
-    :return: A dictionary with the player's stats (e.g. name, class, HP, exp, etc.).
+    :return: A dictionary with the player's stats (e.g. name, class, HP, exp, etc.) and (x, y)-coordinates.
     """
     player = {
         'name': '',
@@ -181,7 +139,9 @@ def make_character():
         'weapon': [],
         'armor': [],
         'exp': 0,
-        'money': 0
+        'money': 0,
+        'x': 0,
+        'y': 24
     }
     return player
 
@@ -341,7 +301,7 @@ def set_class_bonus(character, class_selection):
     """
     Set class/subclass bonuses to character.
 
-    :param character: A dictionary representing the character's stats.
+    :param character: A dictionary representing the character's stats and (x, y)-coordinates.
     :param class_selection: A string representing whether we are adding bonuses via the character's class/subclass.
     :precondition: character must be a dictionary with stats.
     :return:
@@ -385,6 +345,13 @@ def set_class_bonus(character, class_selection):
 
 
 def print_character_stats(character):
+    """
+    Print the character stats on the screen.
+
+    :param character: A dictionary representing the character's stats and (x, y)-coordinates.
+    :precondition: character must be a dictionary with stats and coordinates.
+    :return:
+    """
     stat_length = 14  # Stats are printed as two on each line, 19 characters total
     longest_string = max(len(character['name']), stat_length) + 9
     border = "#" + "=" * longest_string + "#"
@@ -402,6 +369,238 @@ def print_character_stats(character):
     print(f"MAG: {character['stat_mag']:6d} | MDF: {character['stat_mdf']:6d}")
     print(f"SPD: {character['stat_spd']:6d} | LUK: {character['stat_luk']:6d}")
     print(border)
+
+
+def print_room(row, column, character):
+    """
+    Print the room given the row and column.
+
+    #=========================#
+    |F44O444444444444444444444|
+    |========================.|
+    |.........................|
+    |...........#============T|
+    |...........|3333333333333|
+    |...........|3333333333333|
+    |...........|3333333333333|
+    |...........|OOOOO33333333|
+    |...........|3333O33333333|
+    |...........|B333O33333333|
+    |T======#...#=============|
+    |2222222|.................|
+    |2222222|.................|
+    |2222222|.................|
+    |2222222|.................|
+    |2222222|.................|
+    |OOOOOOO|.................|
+    |2222222|.................|
+    |2222222|.................|
+    |222222B|.................|
+    |========================B|
+    |00001111111111O1111111111|
+    |00T01111111111O1111111111|
+    |00001111111111O1111111111|
+    |00001111111111O1111111111|
+    #=========================#
+
+    ---Legend---
+    . - Main Area
+    0 - Starting Area
+    1 - Dungeon 1 (Underground Area)
+    2 - Dungeon 2 (Colorless Dungeon)
+    3 - Dungeon 3 (Mountain)
+    4 - Dungeon 4 (The Dark World)
+
+    B - Boss
+    F - Final Boss
+    O - Organ Man Event
+    T - Town (Buy, Inn)
+
+    :param row: An integer representing the row value on the game board.
+    :param column: An integer representing the column value on the game board.
+    :param character: A dictionary representing the character's stats and (x, y)-coordinates.
+    :precondition: row and column must be integers that are valid row and column values in the game board.
+    :precondition: character must be a dictionary with stats and coordinates.
+    :return: A string representing the room at (row, column).
+    """
+    global colors_dict
+
+    if character['x'] == column and character['y'] == row:
+        return f"{colors_dict['c_blue']}<P>{colors_dict['c_reset']}"
+    # Setting the borders
+    elif (row, column) == (-1, -1):
+        return "╔"
+    elif (row, column) == (-1, 25):
+        return "╗"
+    elif (row, column) == (25, -1):
+        return "╚"
+    elif (row, column) == (25, 25):
+        return "╝"
+    elif (row not in [1, 10, 20] and column in [-1, 25]) or (row, column) == (1, 25) or (row, column) == (10, -1)\
+            or (row, column) == (20, 25):
+        return "║"
+    elif row in [1, 20] and column == -1:
+        return "╠"
+    elif (row, column) == (20, 7):
+        return "═╩═"
+    elif (row, column) == (10, 25):
+        return "╣"
+    elif ((row == 1 or row == 20) and column < 24) or (row == 3 and 12 <= column <= 23)\
+            or (row == 10 and (1 <= column <= 6 or 12 <= column <= 24)) or row in [-1, 25]:
+        return "═══"
+    elif (4 <= row <= 9 and column == 11) or (11 <= row <= 19 and column == 7):
+        return " ║ "
+    elif (row, column) == (3, 11):
+        return " ╔═"
+    elif (row, column) == (10, 7):
+        return "═╗ "
+    elif (row, column) == (10, 11):
+        return " ╚═"
+    # Setting the colors
+    elif 21 <= row <= 24 and column < 4:
+        return f"{colors_dict['c_white']}¤¤¤{colors_dict['c_reset']}"
+    elif (21 <= row <= 24 and 4 <= column <= 24) or (row, column) == (20, 24):
+        return f"{colors_dict['c_magenta']}≡≡≡{colors_dict['c_reset']}"
+    elif 10 <= row <= 19 and column <= 7:
+        return f"{colors_dict['c_yellow']}◊ð≈{colors_dict['c_reset']}"
+    elif 3 <= row <= 9 and 12 <= column <= 24:
+        return f"{colors_dict['c_cyan']}△Λ▴{colors_dict['c_reset']}"
+    elif (row, column) == (1, 24) or 2 <= row <= 19:
+        return f"{colors_dict['c_green']}Îî↑{colors_dict['c_reset']}"
+    elif row == 0:
+        return f"{colors_dict['b_black']}{colors_dict['c_red']}Ξ♰Ψ{colors_dict['c_reset']}"
+    return "   "
+
+
+def print_board(board, character, square_size):
+    """
+    Print the board to the terminal.
+
+    :param board: A dictionary of the game board.
+    :param character: A dictionary representing the character's stats and (x, y)-coordinates.
+    :param square_size: A positive integer representing the width/height of the printed board.
+    :precondition: board has keys which are tuples of row-column coordinates.
+    :precondition: board has values which are coordinate statuses.
+    :precondition: character must be a dictionary with stats and coordinates.
+    :precondition: square_size must be an odd positive integer (to center the player).
+    :postcondition: Print current board.
+    """
+    rows = get_board_rows(board)
+    columns = get_board_columns(board)
+    square_radius = square_size // 2
+    x_left = max(-1, character['x'] - square_radius)
+    x_right = min(columns, character['x'] + square_radius)
+    y_top = max(-1, character['y'] - square_radius)
+    y_bottom = min(rows, character['y'] + square_radius)
+    for row in range(y_top, y_bottom + 1):
+        printed_row = ""
+        for column in range(x_left, x_right + 1):
+            printed_row += print_room(row, column, character)
+        print(printed_row)
+
+
+def convert_direction_choice(dir_input):
+    """
+    Returns the direction input as coordinates.
+
+    :param dir_input: The input given for the direction.
+    :precondition: dir_input is a string and a valid directional input (i.e. in '1234nesw', 'north', 'east', 'south',
+                   or 'west').
+    :postcondition: Determines the (x, y)-coordinates depending on dir_input as a tuple.
+    :return: Tuple (x, y)-coordinates depending on dir_input.
+    >>> convert_direction_choice('1')
+    (0, -1)
+    >>> convert_direction_choice('e')
+    (1, 0)
+    >>> convert_direction_choice('south')
+    (0, 1)
+    >>> convert_direction_choice('4')
+    (-1, 0)
+    """
+    dir_movement = [0, 0]
+    if dir_input in '1n' or dir_input == 'north':
+        dir_movement[1] -= 1
+    elif dir_input in '2e' or dir_input == 'east':
+        dir_movement[0] += 1
+    elif dir_input in '3s' or dir_input == 'south':
+        dir_movement[1] += 1
+    elif dir_input in '4w' or dir_input == 'west':
+        dir_movement[0] -= 1
+    return tuple(dir_movement)
+
+
+def get_user_choice():
+    """
+    Return the user's coordinate choice.
+
+    :return: Tuple (x, y)-coordinates depending on user's input.
+    """
+    print("1. (N)orth\n2. (E)ast\n3. (S)outh\n4. (W)est")
+    dir_input = input("Which direction do you want to go? ").lower()
+    short_list = '1234nesw'
+    coordinate_list = ['north', 'east', 'south', 'west']
+    while (dir_input not in short_list or (dir_input in short_list and len(dir_input) != 1))\
+            and dir_input not in coordinate_list:
+        print("Invalid input.")
+        dir_input = input("Which direction do you want to go? ").lower()
+    return dir_input
+
+
+def is_wall(row, column, character):
+    wall_strings = ["╔", "╗", "╚", "╝", "║", "╩", "═"]
+    for wall_string in wall_strings:
+        if wall_string in print_room(row, column, character):
+            return True
+    return False
+
+
+def validate_move(board, character, direction):
+    """
+    Return whether a player's directional input is possible.
+
+    :param board: A dictionary of the game board.
+    :param character: A dictionary of the player.
+    :param direction: A string which is the given direction.
+    :precondition: board has keys which are tuples of row-column coordinates.
+    :precondition: board has values which are coordinate statuses.
+    :precondition: character has keys/values with board coordinates and current HP.
+    :precondition: direction is a string and a valid directional input (i.e. in '1234nesw', 'north', 'east', 'south',
+                   or 'west').
+    :postcondition: Return whether direction on board is valid for character.
+    :return: Whether direction on board is valid for character.
+    >>> game_board = make_board(4, 4)
+    >>> player = make_character()
+    >>> move_east = convert_direction_choice('e')
+    >>> validate_move(game_board, player, move_east)
+    True
+    >>> move_west = convert_direction_choice('w')
+    >>> validate_move(game_board, player, move_west)
+    False
+    """
+    rows = get_board_rows(board)
+    columns = get_board_columns(board)
+    direction_tuple = convert_direction_choice(direction)
+    new_x = character['x'] + direction_tuple[0]
+    new_y = character['y'] + direction_tuple[1]
+    inbound = 0 <= new_x < columns and 0 <= new_y < rows
+    wall = is_wall(new_y, new_x, character)
+    return inbound and not wall
+
+
+def move_character(character, direction):
+    """
+    Update the character's (x, y)-coordinates.
+
+    :param character: A dictionary of the player.
+    :param direction: A string which is the given direction.
+    :precondition: character has keys/values with board coordinates and current HP.
+    :precondition: direction is a string and a valid directional input (i.e. in '1234nesw', 'north', 'east', 'south',
+                   or 'west').
+    :postcondition: Update character's (x, y)-coordinates via direction.
+    """
+    direction_tuple = convert_direction_choice(direction)
+    character['x'] += direction_tuple[0]
+    character['y'] += direction_tuple[1]
 
 
 def game():
@@ -425,6 +624,17 @@ def game():
         character['subclass'] = set_class(selecting_subclass)
     set_class_bonus(character, 'subclass')
     print_character_stats(character)
+    valid_move = False  # Added here so I don't output the map twice.
+    while True:
+        if not valid_move:  # Added here so I don't output the map twice.
+            print_board(board, character, 7)
+        direction = get_user_choice()
+        valid_move = validate_move(board, character, direction)
+        if valid_move:
+            move_character(character, direction)
+            print_board(board, character, 7)
+        else:
+            print("Can't move here.")
 
 
 def main():
